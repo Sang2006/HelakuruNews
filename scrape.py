bcea338742d3d6b7bde5d0e783ead2f49f56e889
Sangeeth77 <sangeethudayanga123@gmail.com>
2023-03-27 09:13:10 +0530

    update

--- scrape.py
+++ scrape.py
@@ -4,37 +4,35 @@ import json
 import subprocess
 from bs4 import BeautifulSoup
 
-# initialize the post_id variable
-post_id = 97080
+# getting the latest post
+with open("data.json", "r") as f:
+    data = json.load(f)
+
+post_id = data["latest_post"]
 
 while True:
     url = f"https://www.helakuru.lk/esana/news/{post_id}"
     response = requests.get(url)
     
-    # if the response is 404, it means the post_id is not available yet
+    data = {"latest_post" : post_id}
+        
+    with open("data.json", "w") as f:
+            json.dump(data, f)
+    
     if response.status_code == 404:
         print("No new post available yet")
         
-    # if the response is 200, it means there is a new post available
     elif response.status_code == 200:
         print("New post available!")
         
-        # do your scraping here
+
         soup = BeautifulSoup(response.content, "html.parser")
-        
-        # extract the image URL
         image_url = soup.find("meta", property="og:image")["content"]
-        
-        # extract the title
         title = soup.find("meta", property="og:title")["content"]
-        
-        # extract the description
         description = soup.find("meta", property="og:description")["content"]
-        
-        # extract the time
         time = soup.find("meta", itemprop="datePublished")["content"]
+        latest_id = post_id
         
-        # create a dictionary to store the values
         data = {
             "title": title,
             "description": description,
@@ -42,18 +40,15 @@ while True:
             "time": time
         }
         
-        # write the dictionary to a JSON file
+        
         with open("data.json", "w") as f:
             json.dump(data, f)
             
         print("Data written to data.json")
         
-        # call the posting.py script to post the reply
         subprocess.run(["python", "posting.py"])
         
-        # increment the post_id for the next iteration
         post_id += 1
     
-    # sleep for some time before checking again
     t.sleep(60)
     print("Looking for new posts...")
